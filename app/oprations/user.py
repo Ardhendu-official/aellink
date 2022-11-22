c1a62a92359fb9b5487f54522bbf0a9bac7934b0
Ardhendu-official <ardhendudasofficial@gmail.com>
2022-11-23 00:34:05 +0530

    Send TRX

--- app/oprations/user.py
+++ app/oprations/user.py
@@ -137,7 +137,7 @@ def show_user_wallet(hash_id: str , db: Session = Depends(get_db)):
 
 def send_trx(request: sendTron, db: Session = Depends(get_db)):
     user = db.query(DbUser).filter(DbUser.user_address == request.from_account).first()
-    url= "http://localhost:2352/api/v1/tron//wallet/send"
+    url= "http://13.234.52.167:2352/api/v1/tron//wallet/send"
     body = {"from_account": request.from_account,
             "to_account": request.to_account,
             "amount": request.amount,
